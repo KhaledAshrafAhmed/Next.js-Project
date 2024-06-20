a7752efc656a993dc8acfb0434c6375acdbfb138
KhaledAshrafAhmed <khalidashraf0033@gmail.com>
2024-06-21 02:22:16 +0300

    Feature/Bug-Fix
    There was bug concerning the usage of an <a> tag inside an <a> tag in navbar.tsx I resolved it by instead of using typescript I did it as a react component

--- slash-assessment/app/components/Navbar.tsx
+++ slash-assessment/app/components/Navbar.tsx
@@ -1,46 +1,12 @@
-// app/components/Navbar.tsx
-"use client";
-
 import React from 'react';
-import styled from 'styled-components';
-import Link from 'next/link';
 import classes from '../styles/classes.module.css'
-
-const NavbarContainer = styled.nav`
-  display: flex;
-  justify-content: space-around;
-  padding: 1rem 2rem;
-  background-color: #0070f3;
-  color: white;
-  border-radius: 10px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  margin: 1rem auto;
-  max-width: 1200px;
-  text-decoration: none;
-`;
-
-const NavItem = styled.a`
-  color: white;
-  text-decoration: none;
-  font-weight: bold;
-  padding: 0.5rem 1rem;
-  border-radius: 5px;
-
-  &:hover {
-    background-color: #005bb5;
-  }
-`;
 
 const Navbar: React.FC = () => {
   return (
-    <NavbarContainer>
-      <Link href="/" className={`${classes.NoDecoration}`} passHref>
-        <NavItem >Home</NavItem>
-      </Link>
-      <Link href="/create-post" className={`${classes.NoDecoration}`} passHref>
-        <NavItem>Create Post</NavItem>
-      </Link>
-    </NavbarContainer>
+    <nav className={`${classes.navbarcontainer}`}>
+      <a href="/" className={`${classes.navitem}`}>Home</a>
+      <a href="/create-post" className={`${classes.navitem}`}>Create Post</a>
+    </nav>
   );
 };
 
